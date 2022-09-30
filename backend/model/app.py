20dcda6a2dbc87a0a11889e88e45bf06dc112185
Shruti Gupta <72212740+shru1032@users.noreply.github.com>
2022-09-30 23:54:37 +0530

    Update app.py

--- backend/model/app.py
+++ backend/model/app.py
@@ -24,10 +24,12 @@ def predict():
     prediction=model.predict_proba(np.array(final))
     output='{0:.{1}f}'.format(prediction[0][1], 2)
 
-    if output>str(0.2):
-        return render_template('Risk_prediction.html',pred='You have a high risk pregnancy.\nProbable risk is {}'.format(output),bhai="Take care!")
-    else:
-        return render_template('Risk_prediction.html',pred='Your pregnancy is safe.\n Probable risk is {}'.format(output),bhai="You are safe!")
+    if output>str(0.5):
+        return render_template('Risk_prediction.html',pred='You have a high risk pregnancy.\nProbable risk is {}'.format(output),bhai="Take Care!")
+    elif str(0.5)<output<str(0.2):
+        return render_template('Risk_prediction.html',pred='You have a mid risk pregnancy.\n Probable risk is {}'.format(output),bhai="Take Care!")
+    elif output<str(0.2):
+        return render_template('Risk_prediction.html',pred='You have a low risk pregnancy.\n Probable risk is {}'.format(output),bhai="Take Care!")
 
 
 if __name__ == '__main__':
