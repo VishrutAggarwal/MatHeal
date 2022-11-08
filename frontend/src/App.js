75acb9c4c6f89f6d9f931466fb7642cac72047b7
Akanksha Kushwaha <cmakanksha@gmail.com>
2022-11-08 15:51:04 +0530

    update: change routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,26 +15,15 @@ function App() {
     <>
     <HashRouter baseline="/">
       <Routes>
-        <Route path ='/dashboard' element={<PatientDash />}></Route>
-        <Route path ='/communitylogin' element={<CommunityLogin />}></Route>
-        <Route path ='/communitydashboard' element={<CommunityDashBoard />}></Route>
-        <Route path ='/supplierdash/:id' element={<SupplierDash />}></Route>
-        <Route exact path ='/' element={<Chatbot />}></Route>
-        {/* <Route exact path ='/' element = {<Landing />}></Route> */}
+        <Route path ='/#/dashboard' element={<PatientDash />}></Route>
+        <Route path ='/#/communitylogin' element={<CommunityLogin />}></Route>
+        <Route path ='/#/communitydashboard' element={<CommunityDashBoard />}></Route>
+        <Route path ='/#/supplierdash/:id' element={<SupplierDash />}></Route>
+        {/* <Route path ='/#/chatbot' element={<Chatbot />}></Route> */}
+        <Route exact path ='/' element = {<Landing />}></Route>
         {/* <Route exact path ='/' element = {<Test />}></Route> */}
       </Routes>
     </HashRouter>
-
-
-    {/* <Router>
-       <Routes>
-        <Landing />
-        <Authentication />
-        <TeleHealth />
-        <ContactUs />
-         <Footer />
-      </Routes>
-    </Router>  */}
     </>
   );
 }
